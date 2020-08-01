d909c9d1301ee4764a841e716e300e4faf57502c
Josh Herrera <joshherrera09@gmail.com>
2020-08-01 14:45:00 -0700

    updated

---
+++ script.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+// Time element in header
+const today = moment().format('dddd, MMMM Do, YYYY : hh:mm A');
+var DisplayMoment = document.getElementById('currentDay');
+  DisplayMoment.innerHTML = today;
+
+// color of middle column based on past, preset,future
+var textColumn = $('#columns');
+if(today > )
+
+});
+
+
