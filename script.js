a747b48350f2c951c2462cca757a80e96c6d582d
Josh Herrera <joshherrera09@gmail.com>
2020-08-04 13:17:44 -0700

    final

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 //  
 //});
 
-//GLOBALS
+// GLOBALS
 var plannerContainer = document.getElementById('planner-container');
 var planner = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
 
 
-// plannerObj['4'] 
+// plannerObj[''] 
 var plannerObj = {};
 
 jQuery(document).ready(function($){
@@ -23,11 +23,11 @@ var textColumn = $('#columns');
 
 });
 
-
+// Setting to local storage
 function handleSave(event){
   var eventTarget = event.target;
   if (eventTarget.classList.contains('fa-save')) {
-    //filtering out to only handle clicks on our btn divs
+    // filtering out to only handle clicks on btn divs
     let parentDiv = eventTarget.parentElement.parentElement;
     let time = parentDiv.querySelector('.hour').textContent;
     let note = parentDiv.querySelector('textarea').value;
@@ -38,7 +38,7 @@ function handleSave(event){
     
   }
 }
-
+// Fetching local data from storage
 function fetchCalendar() {
   let storedObj = localStorage.getItem('calendar');
   if (storedObj) {
@@ -47,7 +47,7 @@ function fetchCalendar() {
   } 
 }
 
-
+// Setting background color based on past, present, future
 function renderCalendar() {
   fetchCalendar();
   plannerContainer.innerHTML = '';
@@ -55,11 +55,16 @@ function renderCalendar() {
   planner.forEach(function(timeSlot, i){
     var bgColor = 'past';
     var event = '';
+    // .hasOwnProperty() returns boolean to indicating whether the object has the specified property as its own property
+    // reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty#:~:text=The%20hasOwnProperty()%20method%20returns,as%20opposed%20to%20inheriting%20it).
+
     if (plannerObj.hasOwnProperty(timeSlot)) {
       event = plannerObj[timeSlot];
     }
     var hour = moment().format('hA');
+    // Returns the position of the first occurence of a value in a string
     var index = planner.indexOf(hour);
+    // Make sure we don't get return of -1, in case the value to search doesn't occur
     if (index !== -1){
       if (i === index) {
         bgColor = 'present';
@@ -69,6 +74,7 @@ function renderCalendar() {
       }
     }
 
+    // Dynamically creates rows for the planner
     let div = document.createElement('div');
     //template literals
     var template = `
